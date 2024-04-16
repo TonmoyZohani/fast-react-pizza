e3b0942ab49e24b0da0b9f678567bcc15390099c
TonmoyZohani <tonmoyzohani@gmail.com>
2024-04-16 23:40:16 +0600

    implement outlet

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,35 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu from "../src/features/menu/Menu";
 import Cart from "../src/features/cart/Cart";
-import CreateOrder from '../src/features/order/CreateOrder';
-import Order from '../src/features/order';
+import CreateOrder from "../src/features/order/CreateOrder";
+import Order from "../src/features/order/Order";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
-  },
-  {
-    path: "/order/:orderId",
-    element: <Order />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
   },
 ]);
 
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -9,6 +10,7 @@ const AppLayout = () => {
 
       <main>
         <h1>Content</h1>
+        <Outlet/>
       </main>
       <CartOverview />
     </div>
