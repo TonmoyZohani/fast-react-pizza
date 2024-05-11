8518064d67d867d0acfb23ba2f7f0f411eac2c36
TonmoyZohani <tonmoyzohani@gmail.com>
2024-05-11 20:43:32 +0600

    tailwind intgrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,49 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "../src/features/menu/Menu";
-import Cart from "../src/features/cart/Cart";
-import Error from "./ui/Error";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Error from './ui/Error';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
 import CreateOrder, {
   action as createOrderAction,
-} from "../src/features/order/CreateOrder";
-import Order, { loader as orderLoader } from "../src/features/order/Order";
-import AppLayout from "./ui/AppLayout";
-import "./index.css";
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     errorElement: <Error />,
+
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
+      { path: '/cart', element: <Cart /> },
       {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
-        errorElement: <Error />,
         loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
 ]);
 
-const App = () => {
+function App() {
   return <RouterProvider router={router} />;
-};
+}
 
 export default App;
--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
-const Loader = () => {
+function Loader() {
   return (
-    <div className='loader'>Loader</div>
-  )
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
