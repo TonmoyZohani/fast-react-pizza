db482c9f56327628778cd67caedeb4b9f707326c
TonmoyZohani <tonmoyzohani@gmail.com>
2024-05-17 20:36:43 +0600

    clear the cart

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,8 +3,9 @@ import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
 import { useSelector } from "react-redux";
-import { getCart } from "../cart/cartSlice";
+import { clearCart, getCart } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
+import store from '../../utils/store.js';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -135,9 +136,9 @@ export async function action({ request }) {
 
   // If everything is okay, create new order and redirect
 
-  // const newOrder = await createOrder(order);
-
-  // return redirect(`/order/${newOrder.id}`);
+  const newOrder = await createOrder(order);
+  store.dispatch(clearCart());
+  return redirect(`/order/${newOrder.id}`);
 
   return null;
 }
